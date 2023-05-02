474a8c3cc536df0291c5c276f55fe9cd3e26329f
Leang Menghang <125108586+luCiferMG2202@users.noreply.github.com>
2023-05-02 16:06:37 +0700

    Leang Menghang

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -39,7 +39,19 @@
       <!-- end arrival section -->
       
       <!-- product section -->
-     
+      @include ('home.product')
+      <!-- end product section -->
+
+      <!-- subscribe section -->
+      
+      <!-- footer end -->
+      <div class="cpy_">
+         <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
+         
+            Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+         
+         </p>
+      </div>
       <!-- jQery -->
       <script src="home/js/jquery-3.4.1.min.js"></script>
       <!-- popper js -->
