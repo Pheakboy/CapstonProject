cb8a40fd7c5f7a209836257b4fdf35fc849c93fa
Pheakboy <128297342+Pheakboy@users.noreply.github.com>
2023-05-10 16:20:46 +0700

    pheaktra

--- resources/views/home/copyright.blade.php
+++ resources/views/home/copyright.blade.php
@@ -1,5 +1,3 @@
 <div class="cpy_">
-    <p class="mx-auto">© 2023 All Rights Reserved By our team name <a href="https://html.design/">CaptonProject</a><br>
-    </p>
     <p class="mx-auto">CADT😊</p>
  </div>
