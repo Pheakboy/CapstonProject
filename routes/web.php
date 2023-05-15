a112de5cf1d0b1516896823ef950856a1203bc1d
Leang Menghang <125108586+luCiferMG2202@users.noreply.github.com>
2023-05-16 04:58:15 +0700

    Menghang

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,19 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
-
+Route::get('/contact', function () {
+    return view('/home.contact');
+});
+Route::get('/product_nav', function () {
+    return view('/home.product_nav');
+});
+Route::get('/userpage', function () {
+    return view('/home.userpage');
+});
 route::get('/redirect',[HomeController::class,'redirect']);
 
 route::get('view_category',[AdminController::class,'view_category']);
 
 route::post('add_category',[AdminController::class,'add_category']);
+
 
