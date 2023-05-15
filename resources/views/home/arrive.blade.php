5e2ac5f61505f33b124ea1ad73c2cc6727aade39
chhenglun <chhenglun.chuob5482gmail.com>
2023-05-16 02:37:34 +0700

    chhenglun

--- resources/views/home/arrive.blade.php
+++ resources/views/home/arrive.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
        <div class="box">
           <div class="arrival_bg_box">
-             <img src="images/arrival-bg.png" alt="">
+             <img src="images/arrival_1.png" alt="">
           </div>
           <div class="row">
              <div class="col-md-6 ml-auto">
@@ -12,8 +12,8 @@
                    </h2>
                 </div>
                 <p style="margin-top: 20px;margin-bottom: 30px;">
-                   Vitae fugiat laboriosam officia perferendis provident aliquid voluptatibus dolorem, fugit ullam sit earum id eaque nisi hic? Tenetur commodi, nisi rem vel, ea eaque ab ipsa, autem similique ex unde!
-                </p>
+                Host: "Ladies and gentlemen, welcome to our highly anticipated shoe arrival event! We're thrilled to showcase the latest collection of stylish and trendy footwear. Get ready to witness an array of stunning designs and impeccable craftsmanship. Without further ado, let's kick off this exciting show!"
+                </p> 
                 <a href="">
                 Shop Now
                 </a>
