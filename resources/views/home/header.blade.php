202b6f7176189c2e4bc7b97baf0fd6dbd3782fb4
chhenglun <chhenglun.chuob5482gmail.com>
2023-05-19 14:17:39 +0700

    chhenglun

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -60,9 +60,10 @@
   <div class="input-group">
     <input type="text" class="form-control" placeholder="Search">
     <div class="input-group-append">
-      <button class="btn btn-primary" type="submit">Search</button>
+      <button  class="btn btn-danger" type="submit">Search</button>
     </div>
   </div>
+      
 </form>
          
              </ul>
