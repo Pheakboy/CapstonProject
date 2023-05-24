2b2c0de646f632fb6de795ed0621e2bfdda23418
chhenglun <chhenglun.chuob5482gmail.com>
2023-05-24 22:28:03 +0700

    Committing local changes

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -59,15 +59,15 @@
 
                @endauth
                 @endif
-                <form class="form-inline">
-  <div class="input-group">
-    <input type="text" class="form-control" placeholder="Search">
-    <div class="input-group-append">
-      <button  class="btn btn-danger" type="submit">Search</button>
-    </div>
-  </div>
+                  <form class="form-inline">
+                  <div class="input-group">
+                     <input type="text" class="form-control" placeholder="Search">
+                     <div class="input-group-append">
+                        <button  class="btn btn-danger" type="submit">Search</button>
+                     </div>
+                  </div>
       
-</form>
+                  </form>
          
              </ul>
           </div>
