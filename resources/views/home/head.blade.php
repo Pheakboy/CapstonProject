1a75d973c69667094ebb95e3edd32e3d4324bd4a
kunva <kunvath.rim@student.cadt.edu.kh>
2023-06-01 13:04:28 +0700

    edit title & aboutus

--- resources/views/home/head.blade.php
+++ resources/views/home/head.blade.php
@@ -10,7 +10,8 @@
     <meta name="keywords" content="" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <link rel="shortcut icon" href="images/shortcut.png" type="">
+    {{--<link rel="shortcut icon" href="images/shortcut.png" type="">--}}
+    <link rel="icon" href="images/titlelogo.png" type="">
     <title>G_Sport</title>
     <!-- bootstrap core css -->
     <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
@@ -21,5 +22,38 @@
     <!-- responsive style -->
     <link href="home/css/responsive.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <style>
+        body {
+            background: #eee;
+        }
 
+        .name {
+            color: blue;
+        }
+
+        .work {
+            font-weight: bold;
+            font-size: 15px;
+        }
+
+        .about span {
+            font-size: 13px;
+        }
+
+        .v-profile {
+            color: blue;
+            cursor: pointer;
+        }
+
+        .box {
+            -webkit-box-shadow: 13px 12px 5px -10px rgba(196,194,196,0.72);
+            -moz-box-shadow: 13px 12px 5px -10px rgba(196,194,196,0.72);
+            box-shadow: 13px 12px 5px -10px rgba(196,194,196,0.72);
+        }
+
+        .col-md-2 {
+            margin-left: 32px;
+            margin-top: 7px;
+        }
+    </style>
 </head>
